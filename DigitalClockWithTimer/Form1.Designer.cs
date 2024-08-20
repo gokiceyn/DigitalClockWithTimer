36ae9b0f4f157bda1531b8be766e54ab97e372a5
gökçen <gken@WINDOWS-V7FCRNT>
2024-08-21 01:12:04 +0300

    Add project files.

---
+++ DigitalClockWithTimer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DigitalClockWithTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label1.Location = new Point(426, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 128);
+            label1.TabIndex = 0;
+            label1.Text = "0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label2.Location = new Point(215, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 128);
+            label2.TabIndex = 1;
+            label2.Text = "0 ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label3.Location = new Point(12, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 128);
+            label3.TabIndex = 2;
+            label3.Text = "0 ";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label4.Location = new Point(134, 92);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 128);
+            label4.TabIndex = 3;
+            label4.Text = ":";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 72F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            label5.Location = new Point(345, 92);
+            label5.Name = "label5";
+            label5.Size = new Size(75, 128);
+            label5.TabIndex = 4;
+            label5.Text = ":";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            ClientSize = new Size(563, 347);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ DigitalClockWithTimer/Form1.cs
@@ -0,0 +1,29 @@
+namespace DigitalClockWithTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int saat = 0, dakika = 0, saniye = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            label1.Text = saniye.ToString();
+            if (saniye == 60)
+            {
+                dakika++;
+                label2.Text = dakika.ToString();
+                saniye = 0;
+                if (dakika == 60)
+                {
+                    saat += 1;
+                    label3.Text = saat.ToString();
+                    dakika = 0;
+                }
+            }
+
+        }
+    }
+}
